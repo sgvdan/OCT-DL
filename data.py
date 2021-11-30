Added Set Network. Need to collate batches of variance sizes + fix dimensions in feed forward deep sets

data.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 import torch
 import random
@@ -12,21 +13,36 @@ from torchvision import transforms
 
 LABELS = {'HEALTHY': torch.tensor(0), 'SICK': torch.tensor(1)}
 
+
 tomograms_train_transformation = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Resize((496, 1024)),
+    transforms.Resize((496, 1024)),  # TODO: Change this to point to config.input_size
     transforms.Normalize(mean=[10.01453], std=[21.36284])
 ])
-
 tomograms_validation_transformation = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Resize((496, 1024)),
+    transforms.Resize((496, 1024)),  # TODO: Change this to point to config.input_size
     transforms.Normalize(mean=[10.53055], std=[21.35372])
 ])
-
 tomograms_test_transformation = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Resize((496, 1024)),
+    transforms.Resize((496, 1024)),  # TODO: Change this to point to config.input_size
+    transforms.Normalize(mean=[10.35467], std=[18.61051])  # calculated by util.get_dataset_stats
+])
+
+volume_train_transformation = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((496, 1024)),  # TODO: Change this to point to config.input_size
+    transforms.Normalize(mean=[10.01453], std=[21.36284])
+])
+volume_validation_transformation = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((496, 1024)),  # TODO: Change this to point to config.input_size
+    transforms.Normalize(mean=[10.53055], std=[21.35372])
+])
+volume_test_transformation = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((496, 1024)),  # TODO: Change this to point to config.input_size
     transforms.Normalize(mean=[10.35467], std=[18.61051])  # calculated by util.get_dataset_stats
 ])
 
@@ -138,8 +154,9 @@ class DatasetIterator:
 
 
 class E2EVolumeGenerator(Dataset):
-    def __init__(self, cache):
+    def __init__(self, cache, transformations=None):
         self.cache = cache
+        self.transformations = transformations
 
     def get_labels(self):
         return self.cache.get_labels()
@@ -151,7 +168,11 @@ class E2EVolumeGenerator(Dataset):
         return len(self.cache)
 
     def __getitem__(self, idx):
-        return self.cache[idx]
+        volume, *other = self.cache[idx]
+        if self.transformations is not None:
+            volume = self.transformations(volume)
+
+        return volume, *other
 
 
 class BScansGenerator(Dataset):
@@ -171,17 +192,13 @@ class BScansGenerator(Dataset):
     def __getitem__(self, idx):
         tomogram, *other = self.cache[idx]
         if self.transformations is not None:
-            try:
-                tomogram = self.transformations(tomogram)
-            except Exception as e:
-                print(e)
+            tomogram = self.transformations(tomogram)
 
         return tomogram.expand(3, -1, -1), *other
 
 
 def random_split_cache(cache, breakdown):
     """
-
     :param cache: cache to split
     :param breakdown: list of fractions to breakdown the cache according to
     :return: list of caches, generated from input *cache* according to *breakdown*
@@ -208,20 +225,24 @@ def build_volume_cache(cache, path, label):
     :param cache:
     :param path:
     :param label:
-    :param limit:
-    :param config:
     :return:
     """
     counter = 0
     for sample in tqdm(list(Path(path).rglob("*.E2E"))):
         if sample.is_file():
-            for volume in E2E(sample).read_oct_volume():
+            for scan in E2E(sample).read_oct_volume():
+                # Ignore volumes with 0-size
+                validity = [isinstance(tomogram, np.ndarray) for tomogram in scan.volume]
+                if not all(validity):
+                    continue
+
                 try:
-                    cache.append((volume.volume, label))
+                    volume = numpy.moveaxis(scan.volume, 0, 2)  # Works best with Pytorch' transformation
+                    cache.append((volume, label))
                     counter += 1
                 except Exception as ex:
                     print("Ignored volume {0} in sample {1}. An exception of type {2} occurred. \
-                               Arguments:\n{1!r}".format(volume.patient_id, sample, type(ex).__name__, ex.args))
+                               Arguments:\n{1!r}".format(scan.patient_id, sample, type(ex).__name__, ex.args))
                     continue
 
 
@@ -230,7 +251,6 @@ def build_tomograms_cache(cache, path, label):
     :param cache:
     :param path:
     :param label:
-    :param transformations:
     :return:
     """
 
@@ -280,3 +300,10 @@ def make_weights_for_balanced_classes(dataset, classes):
         weights[idx] = weight_per_class[int(label)]
 
     return torch.FloatTensor(weights)
+
+
+def variable_size_collate(batch):
+    data = [volume for volume, _ in batch]
+    labels = [label for _, label in batch]
+    labels = torch.LongTensor(labels)
+    return [data, labels]

experiment3d.py
@@ -2,8 +2,9 @@ import torch
 import random
 import data
 from config import default_config
-from train import train, evaluate
-from data import BScansGenerator, Cache, make_weights_for_balanced_classes
+from set_network import E2ESetNetwork
+from train import evaluate, train
+from data import E2EVolumeGenerator, Cache, make_weights_for_balanced_classes
 from network import get_model_and_optim, load_best_state
 
 import wandb
@@ -30,40 +31,41 @@ class Experiment:
 
         # Set Model, Optimizer & Loss
         self.criterion = torch.nn.functional.cross_entropy
-        self.model, self.optimizer = get_model_and_optim(model_name=self.config.model_name, lr=self.config.lr,
-                                                         device=self.config.device, load_best_model=True,
-                                                         pretrained=False)
+        self.backbone, _ = get_model_and_optim(model_name=self.config.model_name, lr=self.config.lr,
+                                               device=self.config.device, load_best_model=True, pretrained=False)
+        self.model = E2ESetNetwork(self.backbone, 0, 0, len(data.LABELS))
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config.lr)
 
     def buildup_data(self):
-        test_cache = Cache('tomogram-test')
-        validation_cache = Cache('tomogram-validation')
-        train_cache = Cache('tomogram-train')
+        test_cache = Cache('volume-test')
+        validation_cache = Cache('volume-validation')
+        train_cache = Cache('volume-train')
 
         if self.config.refresh_cache:
-            data.build_tomograms_cache(test_cache, 'Data/test/control', data.LABELS['HEALTHY'])
-            data.build_tomograms_cache(test_cache, 'Data/test/study', data.LABELS['SICK'])
+            data.build_volume_cache(test_cache, 'Data/test/control', data.LABELS['HEALTHY'])
+            data.build_volume_cache(test_cache, 'Data/test/study', data.LABELS['SICK'])
 
-            data.build_tomograms_cache(validation_cache, 'Data/validation/control', data.LABELS['HEALTHY'])
-            data.build_tomograms_cache(validation_cache, 'Data/validation/study', data.LABELS['SICK'])
+            data.build_volume_cache(validation_cache, 'Data/validation/control', data.LABELS['HEALTHY'])
+            data.build_volume_cache(validation_cache, 'Data/validation/study', data.LABELS['SICK'])
 
-            data.build_tomograms_cache(train_cache, 'Data/train/control', data.LABELS['HEALTHY'])
-            data.build_tomograms_cache(train_cache, 'Data/train/study', data.LABELS['SICK'])
+            data.build_volume_cache(train_cache, 'Data/train/control', data.LABELS['HEALTHY'])
+            data.build_volume_cache(train_cache, 'Data/train/study', data.LABELS['SICK'])
 
         # Build up Training Dataset
         print("Load Train")
-        train_dataset = BScansGenerator(train_cache, transformations=data.tomograms_train_transformation)
+        train_dataset = E2EVolumeGenerator(train_cache, transformations=data.volume_train_transformation)
         train_weights = make_weights_for_balanced_classes(train_dataset, data.LABELS)
         train_sampler = torch.utils.data.sampler.WeightedRandomSampler(train_weights, len(train_weights))
         train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=self.config.batch_size,
                                                    sampler=train_sampler)
         # Build up Validation Dataset
         print("Load Validation")
-        validation_dataset = BScansGenerator(validation_cache, transformations=data.tomograms_validation_transformation)
+        validation_dataset = E2EVolumeGenerator(validation_cache, transformations=data.volume_validation_transformation)
         validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=self.config.batch_size)
 
         # Build up Test Dataset
         print("Load Test")
-        test_dataset = BScansGenerator(test_cache, transformations=data.tomograms_test_transformation)
+        test_dataset = E2EVolumeGenerator(test_cache, transformations=data.volume_test_transformation)
         test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=self.config.batch_size)
 
         return test_loader, validation_loader, train_loader
@@ -92,15 +94,9 @@ def runner(config):
 
 
 def main():
-    vgg19_config = default_config
-    vgg19_config.model_name = 'vgg19_nominal'
-    with wandb.init(project="OCT-DL", config=vgg19_config):
-        runner(wandb.config)
-
-    resnet_config = default_config
-    resnet_config.model_name = 'resnet18_nominal'
-    with wandb.init(project="OCT-DL", config=resnet_config):
-        runner(wandb.config)
+    # with wandb.init(project="OCT-DL", config=default_config):
+    #     runner(wandb.config)
+    runner(default_config)
 
 
 if __name__ == '__main__':

set_network.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -14,10 +15,10 @@ class PermEqui1_mean(nn.Module):
 
 
 class E2ESetNetwork(nn.Module):
-    def __init__(self, backbone, x_dim, d_dim):
+    def __init__(self, backbone, x_dim, d_dim, num_classes):
         super().__init__()
 
-        self.backbone = backbone
+        self.backbone = torch.nn.Sequential(*list(backbone.children())[:-2])
         self.phi = self.phi = nn.Sequential(
             PermEqui1_mean(x_dim, d_dim),
             nn.ELU(inplace=True),
@@ -32,12 +33,14 @@ class E2ESetNetwork(nn.Module):
             nn.Linear(d_dim, d_dim),
             nn.ELU(inplace=True),
             nn.Dropout(p=0.5),
-            nn.Linear(d_dim, 40),
+            nn.Linear(d_dim, num_classes),
         )
 
     def forward(self, x):
-        features = self.backbone(x)  # B x M x h x w - B=batch size, M=#slices_per_volume, h=height, w=width
-        phi_output = self.phi(features)
+        # TODO: SORT OUT THE DIMENSIONS SO IT WILL BE POSSIBLE TO DO IT WITH BxMxHxW
+        batch_size, slices_num, height, width = x.shape
+        features = self.backbone(x.view(1, batch_size * slices_num, height, width))  # B x M x h x w - B=batch size, M=#slices_per_volume, h=height, w=width
+        phi_output = self.phi(features.view(batch_size, slices_num, height, width))
         sum_output = phi_output.mean(1)
         ro_output = self.ro(sum_output)
         return ro_output